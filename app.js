d32f04fd2997df6ebe70faf3d127c93f0bae0321
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2012-11-24 11:58:04 +0100

    Added ability to retrieve git revision from app.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var nconf = require('nconf');
+var exec = require('child_process').exec;
 
 nconf.argv().env().file(__dirname + '/config/' + (process.env.NODE_ENV || 'development') + '.json');
 
@@ -9,6 +10,20 @@ app.get('/', function(req, res) {
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
   res.end(body);
+});
+
+app.get('/revision', function(req, res) {
+  exec('git rev-parse --short HEAD', function (err, stdout, stderr) {
+    var revision;
+    if (err) {
+      revision = 'Could not read git revision: ' + stderr;
+    } else {
+      revision = stdout;
+    }
+    res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Length', revision.length);
+    res.end(revision);
+  });
 });
 
 var port = nconf.get('port');
