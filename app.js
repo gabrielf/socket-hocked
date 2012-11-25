91330e02dfd94531b0435393b81eab88cecbdcc0
Gabriel Falkenberg <gabriel.falkenberg@gmail.com>
2012-11-25 20:33:17 +0100

    Introduced error (testing rollback).

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var exec = require('child_process').exec;
 
 nconf.argv().env().file(__dirname + '/config/' + (process.env.NODE_ENV || 'development') + '.json');
 
-app.get('/', function(req, res) {
+app.get('/hello', function(req, res) {
   var body = 'Hello World';
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', body.length);
